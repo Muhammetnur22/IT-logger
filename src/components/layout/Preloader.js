8e8954c5e65596cb3011825884696e83fb8f9458
Gurbanov01 <rahmanberdi0611@gmail.com>
2021-08-14 15:01:52 +0500

    latest

---
+++ src/components/layout/Preloader.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Preloader() {
+  return (
+    <div className="proogress blue lighten-4">
+      <div className="indeterminate blue"></div>
+    </div>
+  );
+}
+
+export default Preloader;
